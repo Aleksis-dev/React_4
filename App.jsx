9909afc970cf56b8c61e06c8394bbb30495ddcbc
CrazyIvanovs <lastovskis08@gmail.com>
2025-06-06 20:26:50 +0300

    Added useEffect and a component that uses it and call it

--- App.jsx
+++ App.jsx
@@ -1,11 +1,24 @@
+import { useEffect } from 'react';
 import "./App.css"
 
 function App() {
   return (
     <>
       <h1>Hello world!</h1>
+      <ChildComponent />
     </>
   )
+}
+
+function ChildComponent() {
+  useEffect(() => {
+    console.log("Child mounted");
+
+    return () => {
+      console.log("Child unmounted");
+    }
+  }, []);
+  return <div>I am the child!</div>
 }
 
 export default App;
